4b1a9101ded2f55fecac3676c50d4f925ebe1b44
marthjod@gmail.com <marthjod@gmail.com>
2017-06-29 20:32:16 +0200

    add test for model package

---
+++ model/model_test.go
@@ -0,0 +1,26 @@
+package model_test
+
+import (
+	"github.com/marthjod/ansible-one-inventory/model"
+	"testing"
+)
+
+func TestInventory_String(t *testing.T) {
+	inv := model.Inventory{
+		"web": model.InventoryGroup{
+			"webserver-01",
+			"webserver-02",
+		},
+	}
+
+	expected := `{
+  "web": [
+    "webserver-01",
+    "webserver-02"
+  ]
+}`
+	actual := inv.String()
+	if actual != expected {
+		t.Errorf("String() did no render correctly: Expected:\n%q\nActual:\n%q", expected, actual)
+	}
+}
